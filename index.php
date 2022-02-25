a6f11ac05772c086acf81bb28cbee0cb9b2b0934
Your Name <yan.vi193@gmail.com>
2022-02-25 12:39:23 +0100

    creazione-file

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require_once __DIR__.'/classi/Movie.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+<!--
+    Oggi pomeriggio ripassate i primi concetti di classe e di variabili e
+    metodi d’istanza che abbiamo visto stamattina e create un file index.php in cui:
+    - è definita una classe ‘Movie’
+        => all’interno della classe sono dichiarate delle variabili d’istanza
+        => all’interno della classe è definito un costruttore
+        => all’interno della classe è definito almeno un metodo
+    - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+-->
+</body>
+</html>
